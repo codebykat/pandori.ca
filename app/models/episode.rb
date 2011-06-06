09fed3e7e33b820f52b2613a0808f0bc87d554b4
kat <kat@codebykat.com>
2011-06-06 12:18:47 -0400

    friendly id for episode names

--- app/models/episode.rb
+++ app/models/episode.rb
@@ -1,5 +1,7 @@
 class Episode < ActiveRecord::Base
 
+  has_friendly_id :name, :use_slug => true
+
   has_many :quotes, :dependent => :destroy
   has_and_belongs_to_many :characters, :uniq => :true
 
