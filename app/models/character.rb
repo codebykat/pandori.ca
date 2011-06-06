7ef8ab955161887764fb8d56c8a033eff5a69311
kat <kat@codebykat.com>
2011-06-06 10:14:02 -0400

    nested attributes for photos

--- app/models/character.rb
+++ app/models/character.rb
@@ -6,4 +6,6 @@ class Character < ActiveRecord::Base
   has_and_belongs_to_many :episodes, :uniq => true
   has_many :character_images, :dependent => :destroy
 
+  accepts_nested_attributes_for :character_images, :reject_if => lambda { |t| t['character_image'].nil? }
+
 end
