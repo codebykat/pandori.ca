6c6d1ac5bec59d221941b3a918608034c751b9b4
kat <kat@codebykat.com>
2011-06-06 12:14:26 -0400

    missing image image

--- app/models/character_image.rb
+++ app/models/character_image.rb
@@ -3,7 +3,8 @@ class CharacterImage < ActiveRecord::Base
   belongs_to :character
 
   # paperclip options
-  has_attached_file :photo, :styles => { :small => "150x150>", :large => "320x240>" }
+  has_attached_file :photo, :styles => { :small => "150x150>", :large => "320x240>" },
+                    :default_url => "/images/missing.jpg"
   validates_attachment_presence :photo
   validates_attachment_size :photo, :less_than => 5.megabytes
 
