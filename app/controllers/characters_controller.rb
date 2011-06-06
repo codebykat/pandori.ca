189f72e58bab93a5ef290d92e617f19443a595ff
kat <kat@codebykat.com>
2011-06-06 10:12:22 -0400

    woo styles

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -5,5 +5,26 @@ class CharactersController < ApplicationController
 
   def show
     @character = Character.find(params[:id])
+    @random_quote = @character.quotes.find(:first, :offset => rand(@character.quotes.count))
+  end
+
+  def update
+    @character = Character.find(params[:id])
+
+    #params[:character][:character_images].each do |image|
+    #  img = CharacterImage.create!(image[1])
+    #  @character.character_images << img
+    #end
+    #@character.save!
+
+    #if @character.save!
+    #  flash[:notice] = "successfully updated images"
+    #end
+
+    if @character.update_attributes(params[:character])
+      flash[:notice] = "successfully updated character."
+    #else
+    #render :action => 'edit'
+    end
   end
 end
