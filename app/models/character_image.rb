ff34d2dfa4f9f0d05806e960f5129c3055756f3f
kat <kat@codebykat.com>
2011-06-18 17:40:15 -0400

    added images to character index page

--- app/models/character_image.rb
+++ app/models/character_image.rb
@@ -3,7 +3,10 @@ class CharacterImage < ActiveRecord::Base
   belongs_to :character
 
   # paperclip options
-  has_attached_file :photo, :styles => { :small => "190x190#", :large => "320x240>" },
+  has_attached_file :photo, :styles => {
+                                        :small => "190x190#",
+                                        :tiny => "50x50#",
+                                        :large => "320x240>" },
                     :default_url => "/images/missing.jpg"
   validates_attachment_presence :photo
   validates_attachment_size :photo, :less_than => 5.megabytes
